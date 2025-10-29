78c82b346f93f37b5822759858553d68c52467f5
cdita <179974249+ccdita@users.noreply.github.com>
2025-10-29 18:42:40 -0400

    Add log in feature to ConsoleUI
    Allow users to navigate to the log in screen via ConsoleUI.

--- src/main/java/com/ccdita/myBookstore/ui/ConsoleUI.java
+++ src/main/java/com/ccdita/myBookstore/ui/ConsoleUI.java
@@ -18,16 +18,19 @@ public class ConsoleUI {
     Scanner scanner = new Scanner(System.in);
 
     private MenuUI menuUI;
+    private LoginRegisterUI loginRegisterUI;
     private UserInput userInput;
 
     /**
      * Construct a ConsoleUI instance with constructor injection
      * @param menuUI
+     * @param loginRegisterUI
      * @param userInput
      */
     @Autowired
-    public ConsoleUI(MenuUI menuUI, UserInput userInput) {
+    public ConsoleUI(MenuUI menuUI, LoginRegisterUI loginRegisterUI, UserInput userInput) {
         this.menuUI = menuUI;
+        this.loginRegisterUI = loginRegisterUI;
         this.userInput = userInput;
     }
 
@@ -36,9 +39,24 @@ public class ConsoleUI {
      * Directs which UI components to call based on user input
      */
     public void start() {
-        menuUI.displayMainMenu();
-        userInput.getUserOption(MAIN_MENU_OPTIONS, scanner);
 
+        boolean isRunning = true; // Dictates whether the application is running or not
+        while (isRunning) {
+            // ===== MAIN MENU =====
+            menuUI.displayMainMenu();
+            int userOption = userInput.getUserOption(MAIN_MENU_OPTIONS, scanner);
+
+            switch(userOption) {
+                case 1: // If the user chooses to log in
+                    loginRegisterUI.displayLoginUI();
+                    String username = userInput.getUserString("Username:", scanner);
+                    String password = userInput.getUserString("Password:", scanner);
+                    break;
+                case 3: // Exit the application
+                    isRunning = false;
+                    continue;
+            }
+        }
         scanner.close();
     }
 }
