8a6b6e8142ef02a208996badbe127a94cf09cf49
cdita <179974249+ccdita@users.noreply.github.com>
2025-10-29 19:18:24 -0400

    Add minor change to printed main menu
    Change main menu for cleaner output.

--- src/main/java/com/ccdita/myBookstore/ui/MenuUI.java
+++ src/main/java/com/ccdita/myBookstore/ui/MenuUI.java
@@ -13,10 +13,10 @@ public class MenuUI {
      */
     public void displayMainMenu() {
         System.out.println("Welcome to myBookstore!");
-        System.out.println("==========");
+        System.out.println("----------");
         System.out.println("1. Log in");
         System.out.println("2. Create an account");
         System.out.println("3. Exit");
-        System.out.println("==========");
+        System.out.println("----------");
     }
 }
