d39eba784e65a8e214bee4347ce5b7bf204147a2
cdita <179974249+ccdita@users.noreply.github.com>
2025-10-29 21:14:53 -0400

    Autowire UserDAO, implement findUserByUsername()
    Set up constructor injection for UserDAO in UserManager to streamline
    injection of the dependency into the class. Implement
    findUserByUsername() which calls the appropriate UserDAO method.

--- src/main/java/com/ccdita/myBookstore/processor/UserManager.java
+++ src/main/java/com/ccdita/myBookstore/processor/UserManager.java
@@ -2,6 +2,7 @@ package com.ccdita.myBookstore.processor;
 
 import com.ccdita.myBookstore.datamanagement.UserDAO;
 import com.ccdita.myBookstore.datamanagement.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,16 +12,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserManager {
 
+    private UserDAO userDAO;
+
+    /**
+     * Constructs a UserManager instance with constructor injection
+     * @param userDAO, implementation of the UserDAO interface
+     */
+    @Autowired
+    public UserManager(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
     /**
      * Creates a new user and persists the associated User object to the database
-     * @param userDAO, DAO implementation for persisting given data
      * @param username of user
      * @param password of user
      */
-    public void createUser(UserDAO userDAO, String username, String password) {
+    public void createUser(String username, String password) {
         // Instantiate a new User object
         User newUser = new User(username, password);
         // Persist the User object to the database
         userDAO.save(newUser);
+    }
+
+    /**
+     * Finds the user based on the given username
+     * @param username to query
+     * @return User object, null if there are no/multiple users found
+     */
+    public User findUserByUsername(String username) {
+        return userDAO.findByUsername(username);
     }
 }
