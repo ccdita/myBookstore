d30504cc77036135a228fd9dfe725d235b3acf13
cdita <179974249+ccdita@users.noreply.github.com>
2025-11-02 15:45:49 -0500

    Implement testDeleteUser()
    Implement testDeleteUser() to verify delete()'s functionality.

--- src/test/java/com/ccdita/myBookstore/UserDAOImplTest.java
+++ src/test/java/com/ccdita/myBookstore/UserDAOImplTest.java
@@ -2,6 +2,7 @@ package com.ccdita.myBookstore;
 
 import com.ccdita.myBookstore.datamanagement.UserDAOImpl;
 import com.ccdita.myBookstore.datamanagement.entities.User;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test class for UserDAOImpl Repository class
@@ -18,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Import(UserDAOImpl.class) // Import UserDAOImpl repository class
 public class UserDAOImplTest {
 
+    @Autowired
+    private EntityManager entityManager; // Auto-inject EntityManager
     @Autowired
     private UserDAOImpl userDAOImpl; // Auto-inject UserDAOImpl
     private User testUser;
@@ -44,7 +47,7 @@ public class UserDAOImplTest {
     void testSaveUser() {
         // Test that save() properly saves the given user to the database
         int testId = testUser.getId();
-        User savedUser = userDAOImpl.findById(testId);
+        User savedUser = entityManager.find(User.class, testId);
         assertEquals(testUser, savedUser, "save() does not properly save the given User to the database.");
         assertEquals(testUser.getUsername(), savedUser.getUsername(), "save() does not properly save " +
                 "the given User to the database.");
@@ -52,5 +55,25 @@ public class UserDAOImplTest {
                 "the given User to the database.");
         assertEquals(testUser.getReaderCash(), savedUser.getReaderCash(), "save() does not properly save " +
                 "the given User to the database.");
+    }
+
+    @Test
+    void testDeleteUser() {
+        // Test that delete() properly removes the given user from the database
+        User testUserToDelete = new User("testUserToDelete","testPassword");
+        // Test that the user is not yet in the database
+        assertNull(entityManager.find(User.class, testUserToDelete.getId()), "User is found in database " +
+                "when it should not have yet been saved.");
+
+        // Persist the user to the database
+        entityManager.persist(testUserToDelete);
+        // Test that the user is in the database
+        assertNotNull(entityManager.find(User.class, testUserToDelete.getId()));
+
+        userDAOImpl.delete(testUserToDelete); // Remove the user from the database
+
+        // Test that the user is no longer in the database
+        assertNull(entityManager.find(User.class, testUserToDelete.getId()), "User is found in database " +
+                "it should have been removed.");
     }
 }
