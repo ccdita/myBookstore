556d6e0c3af20a115f018c2674895c2eacdbf63a
cdita <179974249+ccdita@users.noreply.github.com>
2025-11-11 19:06:25 -0500

    Implement TransactionManager
    Implement TransactionManager, which handles transactions (buying and
    selling books). This class now holds methods that involve communicating
    with both the User and Book data management classes, so that
    UserManager and BookManager can focus solely on communicating with their
    respective DAO objects.

---
+++ src/main/java/com/ccdita/myBookstore/processor/TransactionManager.java
@@ -0,0 +1,58 @@
+package com.ccdita.myBookstore.processor;
+
+import com.ccdita.myBookstore.datamanagement.BookDAO;
+import com.ccdita.myBookstore.datamanagement.UserDAO;
+import com.ccdita.myBookstore.datamanagement.entities.Book;
+import com.ccdita.myBookstore.datamanagement.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Manages transaction-related CRUD operations
+ * Abstracts away specific implementations with communicating with the mySQL database
+ */
+@Component
+public class TransactionManager {
+
+    private BookManager bookManager;
+    private UserManager userManager;
+
+    @Autowired
+    public TransactionManager(BookManager bookManager, UserManager userManager) {
+        this.bookManager = bookManager;
+        this.userManager = userManager;
+    }
+
+    /**
+     * Sells a book with the given information and updates the database
+     * @param user, the user currently logged in
+     * @param title of book
+     * @param author of book
+     * @param genre of book
+     * @returns the sold book
+     */
+    public Book sellBook(User user, String title, String author, String genre) {
+        Book bookToSell = bookManager.addBook(title, author, genre);
+        userManager.addReaderCash(user, bookToSell.getPrice());
+        return bookToSell;
+    }
+
+    /**
+     * Gets the book the user wishes to buy and updates the database with its new forSale status
+     * @param userOption, the user's input for the book they would like to buy from a given list
+     * @param user, the current user logged in
+     * @param booksForSale, the list of books for sale
+     * @return the purchased book, otherwise null (if the user cannot afford the book)
+     */
+    public Book buyBook(int userOption, User user, List<Book> booksForSale) {
+        Book bookToBuy = booksForSale.get(userOption - 1); // Get the object of the book the user wants to buy
+        Double remainingReaderCash = userManager.deductReaderCash(user, bookToBuy.getPrice());
+        // If the user can't afford the book, return null
+        if (remainingReaderCash == null) { return null; }
+        // If the user can afford the book, update the user's ReaderCash and the book's forSale status
+        bookManager.placeBookForSale(bookToBuy);
+        return bookToBuy;
+    }
+}
---
+++ src/test/java/com/ccdita/myBookstore/TransactionManagerTest.java
@@ -0,0 +1,67 @@
+package com.ccdita.myBookstore;
+
+import com.ccdita.myBookstore.datamanagement.entities.Book;
+import com.ccdita.myBookstore.datamanagement.entities.User;
+import com.ccdita.myBookstore.processor.BookManager;
+import com.ccdita.myBookstore.processor.TransactionManager;
+import com.ccdita.myBookstore.processor.UserManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test class for TransactionManager
+ */
+public class TransactionManagerTest {
+
+    @Mock // Create a mock object of BookManager
+    private BookManager mockedBookManager;
+    @Mock // Create a mock object of UserManager
+    private UserManager mockedUserManager;
+
+    @InjectMocks // Inject the mocks into TransactionManager
+    private TransactionManager transactionManager;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this); // Initialize mock injection
+    }
+
+    @Test
+    public void testSellBook() {
+        // Test that sellBook() properly handles selling a book
+        String title = "Benelux";
+        String author = "Bob";
+        String genre = "Action";
+        Book bookToSell = new Book(title, author, genre);
+        User user = new User("testUser1", "testPassword1");
+        // Specify return value of mock BookManager object
+        when(mockedBookManager.addBook(title, author, genre)).thenReturn(bookToSell);
+        Book bookSold = transactionManager.sellBook(user, title, author, genre);
+        assertEquals(bookToSell, bookSold, "sellBook() does not sell the correct book.");
+    }
+
+    @Test
+    public void testBuyBook() {
+        // Test that buyBook() properly handles purchasing a book
+        ArrayList<Book> booksForSale = new ArrayList<>();
+        booksForSale.add(new Book("Abracadabra", "Abraham", "Action"));
+        Book bookToBuy = new Book("Benelux", "Bob", "Action");
+        booksForSale.add(bookToBuy);
+        booksForSale.add(new Book("Catharsis", "Catherine", "Comedy"));
+
+        int userOption = 2;
+
+        User user = new User("testUser1", "testPassword1");
+
+        Book bookBought = transactionManager.buyBook(userOption, user, booksForSale);
+        assertEquals(bookToBuy, bookBought, "buyBook() does not purchase the correct book.");
+    }
+}
