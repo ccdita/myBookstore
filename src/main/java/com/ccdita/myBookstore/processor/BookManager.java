ed3d90d6f9237960c7e7bc9597a304d35094f186
cdita <179974249+ccdita@users.noreply.github.com>
2025-11-11 20:05:24 -0500

    Update BookManager
    Move buyBook() to TransactionManager so that BookManager contains
    methods that rely on BookDAOImpl only (buyBook() relies on both
    BookDAOImpl and UserDAOImpl). Update addBook() to return a Book object,
    which will be used in transaction-related methods. Implement
    placeBookForSale() for handling book and database state changes after
    selling a book.

--- src/main/java/com/ccdita/myBookstore/processor/BookManager.java
+++ src/main/java/com/ccdita/myBookstore/processor/BookManager.java
@@ -2,11 +2,9 @@ package com.ccdita.myBookstore.processor;
 
 import com.ccdita.myBookstore.datamanagement.BookDAO;
 import com.ccdita.myBookstore.datamanagement.entities.Book;
-import com.ccdita.myBookstore.datamanagement.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,12 +30,14 @@ public class BookManager {
      * @param title of book
      * @param author of book
      * @param genre of book
+     * @return the new book
      */
-    public void addBook(String title, String author, String genre) {
+    public Book addBook(String title, String author, String genre) {
         // Instantiate a new Book object
         Book newBook = new Book(title, author, genre);
         // Persist the Book object to the database
         bookDAO.save(newBook);
+        return newBook;
     }
 
     /**
@@ -50,20 +50,11 @@ public class BookManager {
     }
 
     /**
-     * Gets the book the user wishes to buy and updates the database with its new forSale status
-     * @param userOption, the user's input for the book they would like to buy from a given list
-     * @param user, the current user logged in
-     * @param booksForSale, the list of books for sale
-     * @return the purchased book, otherwise null (if the user cannot afford the book)
+     * Sets the book's forSale status to true and updates the database
+     * @param bookToBuy, book to update
      */
-    public Book buyBook(int userOption, User user, List<Book> booksForSale) {
-        Book bookToBuy = booksForSale.get(userOption - 1); // Get the object of the book the user wants to buy
-        Double remainingReaderCash = user.deductReaderCash(bookToBuy.getPrice());
-        // If the user can't afford the book, return null
-        if (remainingReaderCash == null) { return null; }
-        // If the user can afford the book, update the user's ReaderCash and the book's forSale status
+    public void placeBookForSale(Book bookToBuy) {
         bookToBuy.setStatus(false); // Set the book's forSale status to false
         bookDAO.update(bookToBuy); // Update the Book object, with the new status, in the database
-        return bookToBuy;
     }
 }
